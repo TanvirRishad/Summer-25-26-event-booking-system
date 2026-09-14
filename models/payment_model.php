565cfc37afdec49eb6c47ca969243b240834ab44
Tanvir2908 <118743779+TanvirRishad@users.noreply.github.com>
2026-09-15 01:18:24 +0600

    Delete models directory

--- models/payment_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-function create_payment($conn, $booking_id, $amount, $method) {
-    $stmt = mysqli_prepare($conn, "INSERT INTO payments (booking_id,amount,method,status) VALUES (?,?,?,'paid')");
-    mysqli_stmt_bind_param($stmt, "ids", $booking_id,$amount,$method);
-    return mysqli_stmt_execute($stmt);
-}
-?>
