3993bfd88f34c57aab475d27b125d43dcaa6b2b3
Tanvir2908 <118743779+TanvirRishad@users.noreply.github.com>
2026-09-15 00:27:03 +0600

    Delete views directory

--- views/admin/attendance.php
+++
@@ -1,5 +0,0 @@
-<?php $title='Attendance'; require 'views/partials/header.php'; ?>
-<h1>Attendance Monitoring</h1><table><tr><th>Booking</th><th>User</th><th>Event</th><th>Status</th><th>Update</th></tr>
-<?php foreach($attendance as $a): ?><tr><td><?=esc($a['booking_code'])?></td><td><?=esc($a['user_name'])?></td><td><?=esc($a['title'])?></td><td><?=esc($a['status'])?></td><td>
-<form class="inline" method="post"><input type="hidden" name="csrf_token" value="<?=csrf_token()?>"><input type="hidden" name="booking_id" value="<?=$a['booking_id']?>"><select name="status"><option>present</option><option>absent</option></select><button class="btn small">Save</button></form></td></tr><?php endforeach; ?></table>
-<?php require 'views/partials/footer.php'; ?>
